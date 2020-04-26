2fe320c6d224fa69c2f1eabca019a88eaff79da8
ishanwardhono <ishan.wardhono@tokopedia.com>
2020-04-26 21:15:05 +0700

    add validation & fix currency format

--- app/models/payslip.rb
+++ app/models/payslip.rb
@@ -3,8 +3,13 @@ class Payslip
     attr_reader :monthly_income_tax
     attr_reader :net_monthly_income
     def initialize(grossmonthly, monthly_tax)
-        @gross_monthly_income = sprintf( "%0.02f", grossmonthly)
-        @monthly_income_tax = sprintf( "%0.02f", monthly_tax)
-        @net_monthly_income = sprintf( "%0.02f", (Float(grossmonthly) - Float(monthly_tax)))
+        @gross_monthly_income = grossmonthly
+        @monthly_income_tax = monthly_tax
+        @net_monthly_income = (grossmonthly - monthly_tax)
+    end
+    def toString()
+        @gross_monthly_income = sprintf( "$%0.02f", @gross_monthly_income)
+        @monthly_income_tax = sprintf( "$%0.02f", @monthly_income_tax)
+        @net_monthly_income = sprintf( "$%0.02f", @net_monthly_income)
     end
 end
